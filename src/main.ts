92131545b9acd2a2dfbc9dac9c062b3d95174afd
mg-e1-mat-tom <matteo_tommasi96@hotmail.com>
2024-03-10 18:43:54 +0100

    added environment config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig).catch((e) =>
-  console.log('error bootstrap', e)
+  console.log('error bootstrap', e),
 );
